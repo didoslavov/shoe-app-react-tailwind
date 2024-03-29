df963a913b772df0cd30bfde110954542426e70e
didoslavov <didkoslawow@gmail.com>
2024-03-30 00:41:50 +0200

    add select component

---
+++ tailwind-shoes-app/src/Components/Select.jsx
@@ -0,0 +1,26 @@
+import { IoIosArrowDown } from "react-icons/io";
+
+const Select = ({ title, options }) => {
+  return (
+    <div className="relative">
+      <select
+        defaultValue={""}
+        className="w-24 appearance-none border border-gray-300 bg-white p-4"
+      >
+        <option disabled hidden value="">
+          {title}
+        </option>
+        {options.map((o) => (
+          <option value={o} key={o}>
+            {o}
+          </option>
+        ))}
+      </select>
+      <div className="flex-center absolute inset-y-0 right-0 pr-3">
+        <IoIosArrowDown />
+      </div>
+    </div>
+  );
+};
+
+export default Select;
--- tailwind-shoes-app/src/Components/ShoeDetail.jsx
+++ tailwind-shoes-app/src/Components/ShoeDetail.jsx
@@ -1,4 +1,6 @@
 import nike1 from "../assets/n1-min.png";
+import { QTY, SIZES } from "../contants.js";
+import Select from "./Select.jsx";
 
 const ShoeDetail = () => {
   return (
@@ -15,7 +17,11 @@ const ShoeDetail = () => {
             "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient."
           }
         </p>
-        <div className="text-3xl font-extrabold md:text-6xl">100$</div>
+        <div className="flex space-x-6">
+          <div className="text-3xl font-extrabold md:text-6xl">100$</div>
+          <Select title={"QTY"} options={QTY} />
+          <Select title={"SIZE"} options={SIZES} />
+        </div>
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to bag
