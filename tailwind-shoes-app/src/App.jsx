ff4b5e304398344cee49a3a1f373cb5c4b4b78b5
didoslavov <didkoslawow@gmail.com>
2024-03-29 18:41:05 +0200

    add hero section

--- tailwind-shoes-app/src/App.jsx
+++ tailwind-shoes-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./Components/Nav.jsx";
+import ShoeDetail from "./Components/ShoeDetail.jsx";
 
 function App() {
   return (
     <div className="p-10 xl:px-24">
       <Nav />
+      <ShoeDetail />
     </div>
   );
 }
---
+++ tailwind-shoes-app/src/Components/ShoeDetail.jsx
@@ -0,0 +1,33 @@
+import nike1 from "../assets/n1-min.png";
+
+const ShoeDetail = () => {
+  return (
+    <section className="flex flex-col space-y-4 lg:flex-row-reverse">
+      <div className="flex-1">
+        <img src={nike1} alt="Image of a shoe" />
+      </div>
+      <div className="flex-1 space-y-6">
+        <h1 className="text-5xl font-black md:text-9xl">Nike Air Max 270</h1>
+        <p className="font-medium md:text-xl">
+          {
+            "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient."
+          }
+        </p>
+        <div className="text-3xl font-extrabold md:text-6xl">100$</div>
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+            Add to bag
+          </button>
+          <a
+            href="#"
+            className="text-lg font-bold underline underline-offset-8"
+          >
+            View details
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShoeDetail;
