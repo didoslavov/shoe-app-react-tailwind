da20fc49e785375d02e37b71bbc5082046722abb
didoslavov <didkoslawow@gmail.com>
2024-03-30 17:41:31 +0200

    add sizes and quantity select menus in cart item component

--- tailwind-shoes-app/src/Components/CartItem.jsx
+++ tailwind-shoes-app/src/Components/CartItem.jsx
@@ -1,22 +1,33 @@
+import { CiTrash } from "react-icons/ci";
+import Select from "./Select.jsx";
+import { QTY, SIZES } from "../contants.js";
 const CartItem = ({ item }) => {
   return (
-    <figure
-      className="
-      flex 
-      cursor-pointer
-    space-x-2
-      bg-gray-50
-    p-2
-    hover:bg-[#DAFFA2]
-      "
-    >
-      <img className="h-24" src={item.src} />
-      <div className="space-y-2">
-        <h3 className="font-bold">{item.title}</h3>
-        <p className="text-sm text-gray-400">{item.description}</p>
+    <div className="cursor-pointer space-y-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
+      <figure className="flex space-x-2">
+        <img className="h-24" src={item.src} />
+        <div className="space-y-2">
+          <h3 className="font-bold">{item.title}</h3>
+          <p className="text-sm text-gray-400">{item.description}</p>
+        </div>
+        <div className="font-bold">{item.price}$</div>
+      </figure>
+      <div className="flex justify-between pl-32">
+        <div className="flex space-x-6">
+          <div className="font-bold">
+            <span>Size</span>
+            <Select title="" options={SIZES} />
+          </div>
+          <div className="font-bold">
+            <span>Qty</span>
+            <Select title="" options={QTY} />
+          </div>
+        </div>
+        <button>
+          <CiTrash size={25} className="text-black" />
+        </button>
       </div>
-      <div className="font-bold">{item.price}$</div>
-    </figure>
+    </div>
   );
 };
 
