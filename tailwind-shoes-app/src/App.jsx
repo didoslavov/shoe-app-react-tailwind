0e15e39e76d89467e4219ce1ca970651500bb8a6
didoslavov <didkoslawow@gmail.com>
2024-03-30 04:09:08 +0200

    add new arrivals component

--- tailwind-shoes-app/src/App.jsx
+++ tailwind-shoes-app/src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "./Components/Nav.jsx";
+import NewArrivalsSection from "./Components/NewArrivalsSection.jsx";
 import ShoeDetail from "./Components/ShoeDetail.jsx";
+import { SHOE_LIST } from "./contants.js";
 
 function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav />
       <ShoeDetail />
+      <NewArrivalsSection items={SHOE_LIST} />
     </div>
   );
 }
---
+++ tailwind-shoes-app/src/Components/NewArrivalsSection.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card.jsx";
+
+function NewArrivalsSection({ items }) {
+  return (
+    <div className="mt-20">
+      <div className="flex-center">
+        <h2 className=" bg-[url('./assets/lines.png')] bg-center text-4xl font-extrabold">
+          NEW ARRIVALS
+        </h2>
+      </div>
+      <div className="mt-10 grid grid-cols-1 justify-between gap-x-6 gap-y-24 md:grid-cols-2 xl:grid-cols-[repeat(3,25%)]">
+        {items.map((i) => (
+          <Card key={i.id} item={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default NewArrivalsSection;
